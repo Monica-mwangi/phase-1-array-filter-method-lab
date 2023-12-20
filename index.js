5ac2d47a0a4aef3d6dcd4d677109dae3ca471c52
Monica <monica@DESKTOP-A3SK9UM.localdomain>
2023-12-20 02:39:33 -0800

    done with assignement

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
+}
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+  function matchName(driver, hometown) {
+    return driver.filter(driver => driver.name ===(hometown));
+  }
+    
+  
+    
+  
+
+  
+    
+        
+
+    
